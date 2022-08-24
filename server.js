491cee4664895ce8edfa53bb83722c0ff9619768
Md Saiful Islam <saif89.2012@gmail.com>
2022-08-24 15:42:15 +0600

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+import jsonServer from "json-server";
+// const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
     static: "./build"
 });
